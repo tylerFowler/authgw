21860347354a2f995e176a8da2c0747b8d0cc302
Tyler Fowler <tylerfowler.1337@gmail.com>
2016-04-15 14:09:32 -0500

    Restructured project

--- verify_authtoken.js
+++
@@ -1,6 +0,0 @@
-/**
-  Verify Auth Token Middleware
-  @description Validates a JWT token with a request and adds the role and configured
-    data points to the request object
-  @author tylerFowler
-**/
