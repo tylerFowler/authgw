21ac421e2d731306bfa868da4373dda420823050
Tyler Fowler <tylerfowler.1337@gmail.com>
2016-04-18 14:36:51 -0500

    Added VerifyToken middleware tests

--- spec/authgw.spec.js
+++ spec/authgw.spec.js
@@ -4,7 +4,7 @@ const test    = require('tape');
 const express = require('express');
 const AuthGW  = require('../lib/authgw');
 
-const createAuthGWHarness = authgw => {
+const createApp = authgw => {
   let app = express();
 
   app.set('authgw', authgw);
@@ -66,4 +66,66 @@ test('AuthGW sync token creation and validation', t => {
     t.equal(unwrapped.role, 'admin', 'Token role should match');
     t.end();
   } catch (err) { t.error(err); t.end(); }
+});
+
+test('Verify token express middleware', t => {
+  const schema = [
+    { name: 'userid', required: true },
+    { name: 'username', required: false }
+  ];
+
+  let authgw = new AuthGW(['user', 'admin'], 'myapp', schema);
+  let verifyTokenFn = AuthGW.Middleware.verifyTokenExpress.call(authgw);
+
+  let runTest = (token, cb) => {
+    let req = {headers: { 'x-access-token': token }};
+    let res = {
+      statusCode: null, msg: null, sendHook: null,
+      status: function status(s) { this.statusCode = s; return this; },
+      sendStatus: function sendStatus(s) {
+        this.statusCode = s;
+        if (this.sendHook) this.sendHook(this);
+        return this;
+      },
+
+      send: function send(msg) {
+        this.msg = msg;
+        if (this.sendHook) this.sendHook(this);
+        return this;
+      }
+    };
+
+    res.sendHook = response => cb(null, req, response);
+    verifyTokenFn(req, res, nextErr => cb(nextErr, req, res));
+  };
+
+  t.plan(6);
+
+  // Regular Successful Verification
+  let userData = { userid: 1234, username: 'tyler' };
+  let validToken = authgw.createTokenSync(userData, 'admin', 10);
+  runTest(validToken, (err, req) => {
+    t.comment('Valid Token Verification');
+    if (err) { t.error(err); t.end(); }
+    t.assert(req._tokenData, 'Data should have been injected into request');
+    t.equal(req._tokenData.userid, userData.userid, 'Data should match the token');
+    t.equal(req.userRole, 'admin', 'Should write the token role to req');
+  });
+
+  // Invalid/Malformed Token
+  runTest('notatoken', (err, req, res) => {
+    t.comment('Invalid/Malformed Token');
+    if (err) { t.error(err); return t.end(); }
+
+    t.equal(res.statusCode, 401, 'Error for malformed token should be 401');
+  });
+
+  // Empty Token Data
+  runTest(null, (err, req) => {
+    t.comment('Empty Token');
+    if (err) { t.error(err); return t.end(); }
+
+    t.notok(req._tokenData, 'Token data should not be written');
+    t.notok(req.userRole, 'Token role should not be written');
+  });
 });
