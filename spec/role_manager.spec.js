7c1dcb13a3c58051ad4183b83d717bfedf1829a9
Tyler Fowler <tylerfowler.1337@gmail.com>
2016-04-18 18:27:02 -0500

    Added Role Mgr restrict to tests

--- spec/role_manager.spec.js
+++ spec/role_manager.spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const test = require('tape');
+const test        = require('tape');
+const Promise     = require('bluebird');
 const RoleManager = require('../lib/role_manager');
 
 test('Role Manager Restriction Middleware', t => {
@@ -11,7 +12,7 @@ test('Role Manager Restriction Middleware', t => {
     let req = { userRole: curRole };
     let res = {
       statusCode: null,
-      sendHook: thisRes => cb(null, req, thisRes),
+      sendHook: thisRes => cb(thisRes),
       sendStatus: function sendStatus(s) {
         this.statusCode = s;
         if (this.sendHook) this.sendHook(this);
@@ -19,6 +20,73 @@ test('Role Manager Restriction Middleware', t => {
       }
     };
 
-    roleMgrFn(req, res, err => cb(err, req, res));
+    roleMgrFn(req, res, () => cb(res));
   };
+
+  let testCases = [];
+
+  let allowedUser = new Promise(resolve => {
+    runWith(['user', 'admin'], 'user', ['user', 'admin'], res => {
+      t.comment('Allowed User');
+      t.notok(res.statusCode, 'Should pass through');
+      resolve();
+    });
+  });
+  testCases.push(allowedUser);
+
+  let notAllowed = new Promise(resolve => {
+    runWith(['admin'], 'user', ['admin'], res => {
+      t.comment('Not Allowed');
+      t.equals(res.statusCode, 403, 'Should return 403 Forbidden');
+      resolve();
+    });
+  });
+  testCases.push(notAllowed);
+
+  let noArray = new Promise(resolve => {
+    runWith(['admin'], 'admin', 'admin', res => {
+      t.comment('Arrayless Value');
+      t.notok(res.statusCode, 'Should not give an error');
+      resolve();
+    });
+  });
+  testCases.push(noArray);
+
+  let allowAll = new Promise(resolve => {
+    t.comment('Wildcard Matching');
+    let wildcardArgs = (role, cb) => [['user', 'admin'], role, '*', cb];
+    let finished = false;
+
+    runWith.apply(
+      null, wildcardArgs('user', res => {
+        t.notok(res.statusCode, 'Should pass through for user role');
+
+        if (finished) resolve();
+        else finished = true;
+      })
+    );
+
+    runWith.apply(
+      null, wildcardArgs('admin', res => {
+        t.notok(res.statusCode, 'Should pass through for admin role');
+
+        if (finished) resolve();
+        else finished = true;
+      })
+    );
+  });
+  testCases.push(allowAll);
+
+  let noReqRole = new Promise(resolve => {
+    runWith(['admin'], null, ['admin'], res => {
+      t.comment('No Request Role');
+      t.equals(res.statusCode, 401, 'Should return 401 Unauthorized');
+      resolve();
+    });
+  });
+  testCases.push(noReqRole);
+
+  Promise.all(testCases)
+  .catch(err => t.error(err))
+  .finally(() => t.end());
 });
