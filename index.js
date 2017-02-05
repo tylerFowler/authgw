dce802690a64859d85500e700b9141ae932aa3d6
Tyler Fowler <tylerfowler.1337@gmail.com>
2017-02-04 19:01:42 -0600

    Correctly export items

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
 module.exports     = require('./lib/authgw');
-module.Middleware  = require('./lib/authgw').Middleware;
-module.RoleManager = require('./lib/role_manager');
-module.Error       = require('./lib/errors');
-module.Flavors     = require('./lib/flavors');
-module.TokenTransport = require('./lib/token_transport').TokenTransport;
-module.TransportMethods = require('./lib/token_transport').TransportMethods;
